38e835c7d7a3b4fe7781801acf570f13b2b6dd6d
gadget <g4dg3t@gmail.com>
2010-09-01 00:00:02 +0200

    auto agent name generation in scenario mode

--- startAgent.sh
+++ startAgent.sh
@@ -4,27 +4,27 @@
 . ./settings/agentProps.sh
 
 #
-# e.g. ./startAgent.sh sample 1 4 1 0 Chuck Norris
+# e.g. ./startAgent.sh agent-1 sample 1 4 0 Chuck Norris
+# AGENT_NAME=agent-1
 # TESTCASE=sample
 # PROCESSES=1
 # THREADS=4
-# AGENT_ID=1
 # AGENT_OFFSET=0
 # TESTCASE_PARAM1=Chuck
 # TESTCASE_PARAM2=Norris
 #
 
-TESTCASE=$1
-PROCESSES=${2:-1}
-THREADS=${3:-1}
-AGENT_ID=${4:-1}
+AGENT_NAME=$1
+TESTCASE=$2
+PROCESSES=${3:-1}
+THREADS=${4:-1}
 export AGENT_OFFSET=${5:-0}
 export TESTCASE_PARAM1=${6:-none}
 export TESTCASE_PARAM2=${7:-none}
 
 if [ -z $TESTCASE ] || [ "$TESTCASE" = "-h" ] || [ "$TESTCASE" = "--help" ]
 then
-  echo Usage: ./startAgent.sh [testcase] [processes] [threads] [agent_offset] [testcase_params]
+  echo Usage: ./startAgent.sh [agent_name] [testcase] [processes] [threads] [agent_offset] [testcase_params]
   exit 1
 fi
 
@@ -43,11 +43,11 @@ then
 fi
 
 echo "Running testcase: $TESTCASE (processes=$PROCESSES / threads=$THREADS)"
-echo "Agent id: $AGENT_ID"
+echo "Agent: $AGENT_NAME"
 echo "Agent offset: $AGENT_OFFSET"
 echo "Testcase params: [$TESTCASE_PARAM1, $TESTCASE_PARAM2]"
 
 cd testcases/$TESTCASE
-$JAVA_HOME/bin/java -Dgrinder.processes="$PROCESSES" -Dgrinder.threads="$THREADS" -Dgrinder.consoleHost="$CONSOLE_HOST" -Dgrinder.consolePort="$CONSOLE_PORT" -Dgrinder.jvm="$JAVA_HOME/bin/java" -Dgrinder.logDirectory="log" -Dgrinder.hostID="agent-$AGENT_ID" -cp $CLASSPATH net.grinder.Grinder grinder.properties
+$JAVA_HOME/bin/java -Dgrinder.processes="$PROCESSES" -Dgrinder.threads="$THREADS" -Dgrinder.consoleHost="$CONSOLE_HOST" -Dgrinder.consolePort="$CONSOLE_PORT" -Dgrinder.jvm="$JAVA_HOME/bin/java" -Dgrinder.logDirectory="log" -Dgrinder.hostID="$AGENT_NAME" -cp $CLASSPATH net.grinder.Grinder grinder.properties
 
 cd ../..
--- startScenario.sh
+++ startScenario.sh
@@ -4,11 +4,14 @@
 
 echo -e "Starting all agents with user: $AGENT_USER\n"
 
+idx=0
 while read line
 do
+  idx=`expr $idx + 1`
+  agent_name="agent-$idx"
   host=`echo $line |cut -d"|" -f1`
   params=`echo $line |cut -d"|" -f2`
-  echo "Starting agent: $host ($params)"
-  nohup ssh -n $AGENT_USER@$host eval "'cd $AGENT_DIR; ./startAgent.sh $params &'" &
+  echo "Starting $agent_name: $host ($params)"
+  nohup ssh -n $AGENT_USER@$host eval "'cd $AGENT_DIR; ./startAgent.sh $agent_name $params &'" &
 done < settings/scenario.conf
 echo "Done."

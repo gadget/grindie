1ab1a85a4dfcdd9ae05f6f99e2c5ee2c1975925e
gadget <g4dg3t@gmail.com>
2010-08-31 23:23:29 +0200

    added unique agent_id to params (passing it to grinder.hostId to ensure that log file can be unique for each agent even on the same machine/directory)

--- startAgent.sh
+++ startAgent.sh
@@ -3,7 +3,26 @@
 . ./settings/envProps.sh
 . ./settings/agentProps.sh
 
-if [ -z $1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
+#
+# e.g. ./startAgent.sh sample 1 4 1 0 Chuck Norris
+# TESTCASE=sample
+# PROCESSES=1
+# THREADS=4
+# AGENT_ID=1
+# AGENT_OFFSET=0
+# TESTCASE_PARAM1=Chuck
+# TESTCASE_PARAM2=Norris
+#
+
+TESTCASE=$1
+PROCESSES=${2:-1}
+THREADS=${3:-1}
+AGENT_ID=${4:-1}
+export AGENT_OFFSET=${5:-0}
+export TESTCASE_PARAM1=${6:-none}
+export TESTCASE_PARAM2=${7:-none}
+
+if [ -z $TESTCASE ] || [ "$TESTCASE" = "-h" ] || [ "$TESTCASE" = "--help" ]
 then
   echo Usage: ./startAgent.sh [testcase] [processes] [threads] [agent_offset] [testcase_params]
   exit 1
@@ -17,23 +36,18 @@ then
   exit 1
 fi
 
-if [ ! -d testcases/$1 ]
+if [ ! -d testcases/$TESTCASE ]
 then
-  echo Testcase $1 cannot be found!
+  echo Testcase $TESTCASE cannot be found!
   exit 1
 fi
 
-PROCESSES=${2:-1}
-THREADS=${3:-1}
-export AGENT_OFFSET=${4:-0}
-export TESTCASE_PARAM1=${5:-none}
-export TESTCASE_PARAM2=${6:-none}
-
-echo "Running testcase: $1 (processes=$PROCESSES / threads=$THREADS)"
+echo "Running testcase: $TESTCASE (processes=$PROCESSES / threads=$THREADS)"
+echo "Agent id: $AGENT_ID"
 echo "Agent offset: $AGENT_OFFSET"
 echo "Testcase params: [$TESTCASE_PARAM1, $TESTCASE_PARAM2]"
 
-cd testcases/$1
-$JAVA_HOME/bin/java -Dgrinder.processes="$PROCESSES" -Dgrinder.threads="$THREADS" -Dgrinder.consoleHost="$CONSOLE_HOST" -Dgrinder.consolePort="$CONSOLE_PORT" -Dgrinder.jvm="$JAVA_HOME/bin/java" -Dgrinder.logDirectory=log -cp $CLASSPATH net.grinder.Grinder grinder.properties
+cd testcases/$TESTCASE
+$JAVA_HOME/bin/java -Dgrinder.processes="$PROCESSES" -Dgrinder.threads="$THREADS" -Dgrinder.consoleHost="$CONSOLE_HOST" -Dgrinder.consolePort="$CONSOLE_PORT" -Dgrinder.jvm="$JAVA_HOME/bin/java" -Dgrinder.logDirectory="log" -Dgrinder.hostID="agent-$AGENT_ID" -cp $CLASSPATH net.grinder.Grinder grinder.properties
 
 cd ../..

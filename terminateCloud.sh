d7359a3849fc3b20140ffb102a617c80f9b12c27
gadget <g4dg3t@gmail.com>
2011-03-24 22:09:13 +0100

    added terminate cloud instances script

---
+++ terminateCloud.sh
@@ -0,0 +1,26 @@
+#
+# Copyright 2010 Adam Dobos
+#
+# This file is part of Grindie.
+#
+# Grindie is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Grindie is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Grindie.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#!/bin/sh
+
+echo -e "\nTerminating EC2 instances"
+ec2-terminate-instances `cat settings/cloud.instances |cut -f2` > ec2-terminate-instances.out 2> ec2-terminate-instances.err
+
+echo -e "Done.\n"
+exit 0

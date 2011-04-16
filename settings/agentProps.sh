81c99cc304ae48617cb1b3eb05caea726d519407
gadget <gadget@thinkpad>
2011-04-16 18:49:34 +0200

    misleading dummy keyname is renamed to show that it should be a private key for ssh sessions

--- settings/agentProps.sh
+++ settings/agentProps.sh
@@ -8,11 +8,10 @@
 export CONSOLE_HOST="localhost"
 export CONSOLE_PORT="6372"
 
-# remote user and directory for the agent machines
-# note: os passwords are not stored, use key based authentication for ssh sessions
+# remote authentication details and directory for the agent machines
 export AGENT_USER="loadtest"
+export AGENT_KEY="~/.ssh/id_rsa"
 export AGENT_DIR="/home/loadtest/loadtest"
-export AGENT_KEY="~/.ssh/id_rsa.pub"
 
 #
 # global variables that are not used by grindie but could make your life easier

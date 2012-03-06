b0c676fca7b901a2fd68152a3f87686ecbb720b9
gadget <g4dg3t@gmail.com>
2012-03-06 13:26:44 +0100

    removed unused proc

--- common/commonhelper.py
+++ common/commonhelper.py
@@ -18,7 +18,6 @@
 #
 
 from java.util import Date
-from java.util.regex import Pattern
 from java.text import SimpleDateFormat
 from net.grinder.script.Grinder import grinder
 
@@ -26,15 +25,6 @@ from net.grinder.script.Grinder import grinder
 def currentDate(format = 'yyyy.MM.dd'):
   df = SimpleDateFormat(format)
   return df.format(Date())
-
-# TODO
-def getValueByRegEx(response, pattern):
-  p = Pattern.compile(pattern)
-  matcher = p.matcher(response.getText())
-  if matcher.find():
-    return matcher.group()
-  else:
-    return ''
 
 # write the response out to disk
 def logResponse(response):
